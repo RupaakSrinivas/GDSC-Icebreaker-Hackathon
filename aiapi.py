c88a37ca45b0e2cb810af2518dcb2ac666f09281
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2023-05-19 11:41:23 +0530

    Added the ChatGPT api and got the output from the api

---
+++ aiapi.py
@@ -0,0 +1,30 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+
+def get_message(personality, query):
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    openai.api_key = api_key
+    if personality == "default":
+        messages = [ {"role": "assistant", "content": "you are a michelin chef, give a recipe for the dish"} ]
+    elif personality == "emotion 1":
+        messages = [ {"role": "assistant", "content": "you are a michelin chef and a sass queen regina, give a recipe for the dish"} ]
+    elif personality == "emotion 2":
+        messages = [ {"role": "assistant", "content": "you are a michelin chef and a passive-aggressive snape, give a recipe for the dish"} ]
+    elif personality == "emotion 3":
+        messages = [ {"role": "assistant", "content": "you are a michelin chef and an overly happy geet, give a recipe for the dish"} ]
+    elif personality == "emotion 4":
+        messages = [ {"role": "assistant", "content": "you are a michelin chef and an old and sad chef, give a recipe for the dish"} ]        
+    messages.append(
+        {"role": "user", "content": query},
+    )
+    chat = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    reply = str(chat.choices[0].message.content)
+    reply = reply.replace("\n","<br>")
+    print(reply)
+    return str(reply)
--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, request, render_template
+from aiapi import get_message
+import asyncio
+
 
 app=Flask(__name__)
+
+async def send_message(personality, query):
+    try:
+        response = get_message(personality, query)
+        print("response : ",response)
+        return response
+    except Exception as e:
+        print(e)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/submit', methods =['POST'])
-def submit():
+async def submit():
     personality = request.form.get('personality')
     query = request.form.get('dish')
-
-    print(personality)
-    print(query)
-
-    return 'Form submitted successfully'
+    reply = await send_message(personality, query)
+    return str(reply)
 
 if __name__ == '__main__':
-    app.run()
+    asyncio.run(app.run(debug="True"))
